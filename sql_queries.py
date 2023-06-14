1005bf002fce2b538fdd44aaab8fa4f66148bf17
Killian Levacher <killian.levacher@ibm.com>
2023-06-14 11:47:50 +0100

    staging table loading fine

--- sql_queries.py
+++ sql_queries.py
@@ -18,29 +18,8 @@ time_table_drop = "DROP TABLE IF EXISTS time_table;"
 
 # CREATE TABLES to create all tables
 
-# CREATE TABLE "sporting_event_ticket" (
-#     "id" double precision DEFAULT nextval('sporting_event_ticket_seq') NOT NULL,
-#     "sporting_event_id" double precision NOT NULL,
-#     "sport_location_id" double precision NOT NULL,
-#     "seat_level" numeric(1,0) NOT NULL,
-#     "seat_section" character varying(15) NOT NULL,
-#     "seat_row" character varying(10) NOT NULL,
-#     "seat" character varying(10) NOT NULL,
-#     "ticketholder_id" double precision,
-#     "ticket_price" numeric(8,2) NOT NULL
-# );
 
-# {"num_songs": 1, 
-# "artist_id": "ARJIE2Y1187B994AB7", 
-# "artist_latitude":null, 
-# "artist_longitude":null, 
-# "artist_location": "", 
-# "artist_name": "Line Renaud", 
-# "song_id": "SOUPIRU12A6D4FA1E1", 
-# "title": "Der Kleine Dompfaff", 
-# "duration": 152.92036, 
-# "year": 0}
-
+########################### STAGING TABLES
 
 staging_songs_table_create = ("""
     CREATE TABLE "staging_song_table" (
@@ -58,28 +37,52 @@ staging_songs_table_create = ("""
                               
 staging_events_table_create= ("""
     CREATE TABLE "staging_event_table" (
-    "artist" character varying(15) ,
-    "auth" character varying(15) ,
-    "firstName" character varying(15) ,
-    "gender" character varying(15) ,
+    "artist" varchar,
+    "auth" varchar,
+    "firstName" varchar,
+    "gender" varchar,
     "itemInSession" integer ,
-    "lastName" character varying(15) ,
+    "lastName" varchar,
     "length" double precision ,
-    "level" character varying(15),
-    "location" character varying(50),
-    "method" character varying(15),
-    "page" character varying(15),
+    "level" varchar,
+    "location" varchar,
+    "method" varchar,
+    "page" varchar,
     "registration" double precision,
     "sessionId" integer,
-    "song" character varying(15),
+    "song" varchar,
     "status" integer,
     "ts" BIGINT,
-    "userAgent" character varying(15),
+    "userAgent" varchar,
     "userId" integer 
 );
 """)
+                              
 
-# ### **Fact Table**
+staging_events_copy = ("""
+    COPY staging_event_table
+    FROM {}
+    iam_role {}
+    region 'us-west-2'
+    TIMEFORMAT AS 'epochmillisecs'
+    JSON {};
+""").format(config['S3']['LOG_DATA'], 
+            config['IAM_ROLE']['ARN'],
+            config['S3']['LOG_JSONPATH'])
+
+
+
+staging_songs_copy = ("""
+copy staging_song_table from {} 
+iam_role {}
+format as JSON 'auto'
+region 'us-west-2'
+TIMEFORMAT AS 'epochmillisecs';
+""").format(config['S3']['SONG_DATA'], 
+            config['IAM_ROLE']['ARN'])
+
+
+################################# FACT TABLES
 
 # 1. **songplays** - records in event data associated with song plays i.e. records with page `NextSong`
 #     - *songplay_id, start_time, user_id, level, song_id, artist_id, session_id, location, user_agent*
@@ -161,66 +164,7 @@ time_table_create = ("""
     "weekday" double precision
 );
 """)
-                     
-#                          staging_songs_table_create = ("""
-#     CREATE TABLE "staging_song_table" (
-#     "artist_id" character varying(15) NOT NULL,
-#     "artist_latitude" character varying(15) NOT NULL,
-#     "artist_longitude" character varying(15) NOT NULL,
-#     "artist_name" character varying(15) NOT NULL,
-#     "song_id" character varying(15) NOT NULL,
-#     "title" character varying(15) NOT NULL,
-#     "duration" double precision NOT NULL,
-#     "year" integer
-# );
-# """)
-
-                              
-# staging_events_table_create= ("""
-#     CREATE TABLE "staging_event_table" (
-#     "artist" character varying(15) ,
-#     "auth" character varying(15) ,
-#     "firstName" character varying(15) ,
-#     "gender" character varying(15) ,
-#     "itemInSession" integer ,
-#     "lastName" character varying(15) ,
-#     "length" double precision ,
-#     "level" character varying(15),
-#     "location" character varying(15),
-#     "method" character varying(15),
-#     "page" character varying(15),
-#     "registration" double precision,
-#     "sessionId" integer,
-#     "song" character varying(15),
-#     "status" integer,
-#     "ts" BIGINT,
-#     "userAgent" character varying(15),
-#     "userId" integer 
-# );
-# """)
-
-# COPY Queries from S3 bucket into STAGING TABLES
-# [IAM_ROLE]
-# ARN='project2User'
-# [S3]
-# LOG_DATA='s3://udacity-dend/log_data'
-
-
-# COPY staging_events
-#     FROM {}
-#     iam_role '{}'
-#     region 'us-west-2'
-#     TIMEFORMAT AS 'epochmillisecs'
-#     JSON {};
-
-# load partitioned data into the cluster
-# from 2-cloudDataWarehouses/4- implementing A DataWarehouseOnAWS/L3 Exercise 3 - Parallel ETL - Solution.ipynb
-# qry = """
-#     copy sporting_event_ticket from 's3://udacity-labs/tickets/split/part'
-#     credentials 'aws_iam_role={}'
-#     gzip delimiter ';' compupdate off region 'us-west-2';
-# """.format(DWH_ROLE_ARN)
-
+            
 
 # SELECT * FROM stl_query
 
@@ -230,49 +174,9 @@ time_table_create = ("""
 # WHERE query = <query ID>;
 
 
-staging_events_copy = ("""
-""")
                        
-# staging_events_copy = ("""
-#     COPY staging_event_table
-#     FROM {}
-#     iam_role {}
-#     region 'us-west-2'
-#     TIMEFORMAT AS 'epochmillisecs'
-#     JSON {};
-# """).format(config['S3']['LOG_DATA'], 
-#             config['IAM_ROLE']['ARN'],
-#             config['S3']['LOG_JSONPATH'])
 
 
-
-# staging_songs_copy = ("""
-#     COPY staging_song_table
-#     FROM {}
-#     iam_role {}
-#     region 'us-west-2'
-#     TIMEFORMAT AS 'epochmillisecs';
-# """).format(config['S3']['SONG_DATA'], 
-#             config['IAM_ROLE']['ARN'])
-
-
-staging_songs_copy = ("""
-copy staging_song_table from {} 
-iam_role {}
-format as JSON 'auto'
-region 'us-west-2'
-TIMEFORMAT AS 'epochmillisecs';
-""").format(config['S3']['SONG_DATA'], 
-            config['IAM_ROLE']['ARN'])
-
-
-# staging_songs_copy = ("""
-#     copy staging_songs from {}
-#     credentials aws_iam_role={}'
-#     format as JSON 'auto'
-#     region 'us-west-2'
-# """).format(config['S3']['SONG_DATA'], 
-#             config['IAM_ROLE']['ARN'])
 
 # INSERT QUERIES to create FINAL TABLES
 
