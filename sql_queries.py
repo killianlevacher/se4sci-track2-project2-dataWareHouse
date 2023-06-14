ffc510b20b55d74ecf84fbceecf8dfd349c8a81d
Killian Levacher <killian.levacher@ibm.com>
2023-06-14 10:24:17 +0100

    create table working without any bug

--- sql_queries.py
+++ sql_queries.py
@@ -95,20 +95,109 @@ staging_events_table_create= ("""
 # 4. **time** - timestamps of records in **songplays** broken down into specific units
 #     - *start_time, hour, day, week, month, year, weekday*
 
+
+##### TODO Make sure to create primary keys
+# TODO find out what start time is from ts in 'epochmillisecs'
+# TODO songplay_id should be auto generated 
 songplay_table_create = ("""
+    CREATE TABLE "songplay_table" (
+    "-----songplay_id" BIGINT NOT NULL,
+    "start_time" BIGINT,
+    "user_id" integer NOT NULL,
+    "level" character varying(15) NOT NULL,
+    "song_id" character varying(15) NOT NULL,
+    "artist_id" character varying(15) NOT NULL,
+    "session_id" integer,
+    "location" character varying(15),
+    "user_agent" character varying(15)
+);
 """)
+                         
+
 
 user_table_create = ("""
+    CREATE TABLE "user_table" (
+    "user_id" integer,
+    "first_name" character varying(15) NOT NULL,
+    "last_name" character varying(15) NOT NULL,
+    "gender" character varying(15) NOT NULL,
+    "level" character varying(15) NOT NULL
+);
 """)
+                     
 
 song_table_create = ("""
+    CREATE TABLE "song_table" (
+    "song_id" character varying(15) NOT NULL,
+    "title" character varying(15) NOT NULL,
+    "artist_id" character varying(15) NOT NULL,
+    "year" integer,
+    "duration" double precision
+);
 """)
+                     
+
 
 artist_table_create = ("""
+    CREATE TABLE "artist_table" (
+    "artist_id" character varying(15) NOT NULL,
+    "artist_name" character varying(15) NOT NULL,
+    "location" character varying(15) NOT NULL,
+    "lattitude" character varying(15) NOT NULL,
+    "longitude" character varying(15) 
+);
 """)
+                       
 
+# TODO find out what start time is from ts in 'epochmillisecs'
 time_table_create = ("""
+    CREATE TABLE "time_table" (
+    "start_time" BIGINT,
+    "hour" integer,
+    "day" integer,
+    "week" integer,
+    "month" integer,
+    "year" integer,
+    "weekday" double precision
+);
 """)
+                     
+#                          staging_songs_table_create = ("""
+#     CREATE TABLE "staging_song_table" (
+#     "artist_id" character varying(15) NOT NULL,
+#     "artist_latitude" character varying(15) NOT NULL,
+#     "artist_longitude" character varying(15) NOT NULL,
+#     "artist_name" character varying(15) NOT NULL,
+#     "song_id" character varying(15) NOT NULL,
+#     "title" character varying(15) NOT NULL,
+#     "duration" double precision NOT NULL,
+#     "year" integer
+# );
+# """)
+
+                              
+# staging_events_table_create= ("""
+#     CREATE TABLE "staging_event_table" (
+#     "artist" character varying(15) ,
+#     "auth" character varying(15) ,
+#     "firstName" character varying(15) ,
+#     "gender" character varying(15) ,
+#     "itemInSession" integer ,
+#     "lastName" character varying(15) ,
+#     "length" double precision ,
+#     "level" character varying(15),
+#     "location" character varying(15),
+#     "method" character varying(15),
+#     "page" character varying(15),
+#     "registration" double precision,
+#     "sessionId" integer,
+#     "song" character varying(15),
+#     "status" integer,
+#     "ts" BIGINT,
+#     "userAgent" character varying(15),
+#     "userId" integer 
+# );
+# """)
 
 # COPY Queries from S3 bucket into STAGING TABLES
 # [IAM_ROLE]
